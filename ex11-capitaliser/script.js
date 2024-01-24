a847a55db27d51c60135d9c3ba193ba6b82c3794
ameliapham <ptt.huong.8895@gmail.com>
2024-01-24 23:49:04 +0100

    Update exercice 11

--- ex11-capitaliser/script.js
+++ ex11-capitaliser/script.js
@@ -1,9 +1,51 @@
-function titleCase(str) {
-    return str.toLowerCase().replace(/(^|\s)\S/g, function(L) {
-      return L.toUpperCase()
-    })
-  }
-  
-const x = titleCase("I'm a little tea pot")
+// Take a word and return a capitalized word.
+// This mean that the first character is uppercase and the rest lowercase.  
+//
+// Example:
+//  capitalize("coucou") -> "Coucou"
+//  capitalize("coucoU") -> "Coucou"
+//  capitalize("coucoU huong") -> "Coucou huong"
+function capitalize(word) {
+
+    let out = "";
+
+    for (const char of word) {
+        if (out === "") {
+            out = char.toUpperCase();
+        } else {
+            out = out + char.toLowerCase();
+        }
+    }
+
+    return out;
+}
+
+function titleCase(sentence) {
+
+    /*
+    // "i'm a littLE tea pot".split(" ") -> [ "i'm", "a", "littLE", "tea", "pot"]
+    const words = sentence.split(" ");
+
+    // [ "i'm", "a", "littLE", "tea", "pot"] -> [ "I'm", "A", "Little", "Tea", "Pot" ]
+    const capitalizedWords = words.map(word => capitalize(word));
+
+    // [ "I'm", "A", "Little", "Tea", "Pot" ] -> "I'm A Little Tea Pot"
+    const out = capitalizedWords.join(" ");
+
+    return out;
+    */
+
+    return sentence
+        .split(" ")
+        .map(capitalize)
+        .join(" ");
+
+
+
+
+}
+
+const x = titleCase("i'm a littLE tea pot")
+
+//"I'm A Little Tea Pot"
 console.log(x)
-  
