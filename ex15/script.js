a2d48b0b2ab9562ab287bdfbb58cd518eaeee3b8
ameliapham <ptt.huong.8895@gmail.com>
2024-01-27 18:45:18 +0100

    2nd special cas in ex15

--- ex15/script.js
+++ ex15/script.js
@@ -9,12 +9,16 @@ function getIndexToIns(arr, num){
     if (arr.length === 0){
         return 0
     }
-    
+
     // Sort the array
     let sortedArr = arr.sort((a,b) => {
         return a-b
     })
     console.log(sortedArr)
 
+    // Special cas 2 : If num is greater than the last element of array, return the index of the last element + 1 (the length of the array)
+    if (num > sortedArr[sortedArr.length - 1]){
+        return sortedArr.length
+    }
 
 }
