552c7195a486056071a602af4acb294961cd0fed
ameliapham <ptt.huong.8895@gmail.com>
2024-01-27 18:41:04 +0100

    Special cas in ex15

--- ex15/script.js
+++ ex15/script.js
@@ -5,10 +5,16 @@
  */
 
 function getIndexToIns(arr, num){
-
+    // Special cas 
+    if (arr.length === 0){
+        return 0
+    }
+    
     // Sort the array
     let sortedArr = arr.sort((a,b) => {
         return a-b
     })
     console.log(sortedArr)
+
+
 }
