8dfa1b30f4da63f5ebd3b71c6e3cfb10b1a98946
ameliapham <ptt.huong.8895@gmail.com>
2024-01-27 18:40:26 +0100

    Sort the array in ex15

--- ex15/script.js
+++ ex15/script.js
@@ -1,5 +1,14 @@
 /**
- * @param {string} arr 
+ * @param {string} arr  
  * @param {number} num
  * @returns {boolean} 
  */
+
+function getIndexToIns(arr, num){
+
+    // Sort the array
+    let sortedArr = arr.sort((a,b) => {
+        return a-b
+    })
+    console.log(sortedArr)
+}
