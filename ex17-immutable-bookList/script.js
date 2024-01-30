3f2f91ed2c1506b35d158f15f9239d6dfd2e5143
ameliapham <ptt.huong.8895@gmail.com>
2024-01-30 23:36:49 +0100

    Exercise 17 Immutable Array Operations

---
+++ ex17-immutable-bookList/script.js
@@ -0,0 +1,14 @@
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+
+
+
+
+
+// Test function 
+const x = add(bookList, "A Brief History of Time")
+console.log(x)
+
+const y = remove(bookList, "On The Electrodynamics of Moving Bodies")
+console.log(y)
