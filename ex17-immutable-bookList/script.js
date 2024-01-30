0f7f010e78cfb138f276ad2ec331a1c14848e7ea
ameliapham <ptt.huong.8895@gmail.com>
2024-01-30 23:42:35 +0100

    Function removing book

--- ex17-immutable-bookList/script.js
+++ ex17-immutable-bookList/script.js
@@ -7,9 +7,15 @@ function add(arr, bookName){
     return newArr
 }
 
+function remove(arr, bookName){
+    let newArr = [...arr]
+    const bookIndex = newArr.indexOf(bookName)
 
-
-
+    if (bookIndex >= 0 ){
+        newArr.splice(bookIndex, 1)
+        return newArr
+    }
+}
 
 // Test function 
 const x = add(bookList, "A Brief History of Time")
