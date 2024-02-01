e4f652b7435e1c3f4cd9fca9705971e087c88ea0
ameliapham <ptt.huong.8895@gmail.com>
2024-02-01 23:54:40 +0100

    Fixing misunderstanding fault: By default, .sort() sorts array elements as if they were strings and sorts by lexicographic, not their numeric value.

--- ex20-sorts-elements/sortNumber.js
+++ ex20-sorts-elements/sortNumber.js
@@ -1,10 +1,9 @@
 // Sorting number by ascending order
 function ascendingOrder(arr) {
-    // return arr.sort((a,b) => a-b);
-    return arr.sort();
+    return arr.sort((a,b) => a-b);
 }
 
-const x = ascendingOrder([1, 5, 2, 3, 4]);
+const x = ascendingOrder([1, 30, 4, 21, 100000]);
 console.log(x)
 
 // Sorting number by descending order
@@ -12,5 +11,5 @@ function descendingOrder(arr){
     return arr.sort((a,b) => b-a)
 }
 
-const y = descendingOrder([1, 5, 2, 3, 4]);
+const y = descendingOrder([1, 30, 4, 21, 100000]);
 console.log(y)
