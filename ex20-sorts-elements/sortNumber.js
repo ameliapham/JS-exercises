815dd7bafb604e860d2fd16b9168a5c221ba3ec8
ameliapham <ptt.huong.8895@gmail.com>
2024-02-01 20:36:16 +0100

    Ex20 Sorting number by descending order

--- ex20-sorts-elements/sortNumber.js
+++ ex20-sorts-elements/sortNumber.js
@@ -5,3 +5,11 @@ function ascendingOrder(arr) {
 
 const x = ascendingOrder([1, 5, 2, 3, 4]);
 console.log(x)
+
+// Sorting number by descending order
+function descendingOrder(arr){
+    return arr.sort((a,b) => b-a)
+}
+
+const y = descendingOrder([1, 5, 2, 3, 4]);
+console.log(y)
