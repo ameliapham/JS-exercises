9d7a168238a228994fb18424ee93fda1ae617497
ameliapham <ptt.huong.8895@gmail.com>
2024-02-01 23:34:17 +0100

    Ex20 Sorting characters by descending order

--- ex20-sorts-elements/sortCharacters.js
+++ ex20-sorts-elements/sortCharacters.js
@@ -5,3 +5,13 @@ function ascendingAlpha(arr) {
 
 const x = ascendingAlpha(['l', 'h', 'z', 'b', 's']);
 console.log(x)
+
+// Sorting characters by descending order
+function descendingAlpha(arr) {
+    return arr.sort((a,b) => {
+        return a === b ? 0 : a < b ? 1 : -1
+    });
+}
+
+const y = descendingAlpha(['l', 'h', 'z', 'b', 's']);
+console.log(y)
