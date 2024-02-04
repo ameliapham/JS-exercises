e1783c80ed17b26d2f8550128ab2d0d71b30d1a7
ameliapham <ptt.huong.8895@gmail.com>
2024-02-04 23:20:34 +0100

    Updating the comments

--- ex25-mutations/script-v1.js
+++ ex25-mutations/script-v1.js
@@ -1,5 +1,4 @@
-// Return true if the string in the first element of the array 
-// contains all of the letters of the string in the second element of the array.
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
 
 function mutation(arr) {
 
--- ex26-chunky-monkey/script.js
+++ ex26-chunky-monkey/script.js
@@ -1,13 +1,13 @@
-// Write a function that splits an array (first argument) into groups the length of size (second argument)
-// and returns them as a two-dimensional array.
+// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
 
-function chunkArrayInGroups(arr, size){
+function chunkArrayInGroups(arr, size) {
     let newArr = []
 
-    for (let i=0; i<arr.length; i+=size){
-        let sliceArr = arr.slice(i, i+size)
+    for (let i = 0; i < arr.length; i += size) {
+        let sliceArr = arr.slice(i, i + size)
         newArr.push(sliceArr)
     }
+
     return newArr
 }
 
