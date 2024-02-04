a45db0c9b42322ff4bafc4764814beed598eede6
ameliapham <ptt.huong.8895@gmail.com>
2024-02-04 22:36:57 +0100

    Ex25 2nd method using .includes()

---
+++ ex25-mutations/script-v2.js
@@ -0,0 +1,20 @@
+// Return true if the string in the first element of the array 
+// contains all of the letters of the string in the second element of the array.
+
+function mutation(arr) {
+
+    const arr1 = arr[0].toLowerCase()
+
+    const arr2 = arr[1].toLowerCase()
+
+    for (let i = 0; i < arr2.length; i++) {
+        if (!arr1.includes(arr2[i])) {
+            return false
+        }
+    }
+    return true
+
+}
+
+const x = mutation(["hello", "oHell"]);
+console.log(x)
