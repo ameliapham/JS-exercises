ad6cdde41ab9dd4f7952b24221f649246960d975
ameliapham <ptt.huong.8895@gmail.com>
2024-02-04 23:15:47 +0100

    Ex26 Chunky monkey

---
+++ ex26-chunky-monkey/script.js
@@ -0,0 +1,15 @@
+// Write a function that splits an array (first argument) into groups the length of size (second argument)
+// and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size){
+    let newArr = []
+
+    for (let i=0; i<arr.length; i+=size){
+        let sliceArr = arr.slice(i, i+size)
+        newArr.push(sliceArr)
+    }
+    return newArr
+}
+
+const x = chunkArrayInGroups(["a", "b", "c", "d", "e", "f", "g"], 2);
+console.log(x)
