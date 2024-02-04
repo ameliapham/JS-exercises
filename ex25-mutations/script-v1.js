6672eed6ab2f73fd0f3eca371b039477b8e938aa
ameliapham <ptt.huong.8895@gmail.com>
2024-02-04 22:15:01 +0100

    Ex25 Improving the function

--- ex25-mutations/script-v1.js
+++ ex25-mutations/script-v1.js
@@ -4,23 +4,26 @@
 function mutation(arr) {
 
     const arr1 = arr[0].split("").map(char => char.toLowerCase())
-  
+
     const arr2 = arr[1].split("").map(char => char.toLowerCase())
-  
-    let newArr2 = []
-  
+
+    // let newArr2 = []
+
     for (let i = 0; i < arr2.length; i++) {
-      if (arr1.every(char => arr2[i] !== char)) {
-        newArr2.push(arr2[i]);
-      }
+        if (arr1.every(char => arr2[i] !== char)) {
+            // newArr2.push(arr2[i]);
+            return false
+        }
     }
-    
+    return true
+
+    /*
     if (newArr2.length === 0) {
       return true
     } else {
       return false
-    }
-  }
-  
-  const x = mutation(["hello", "hey"]);
-  console.log(x)
+    } */
+}
+
+const x = mutation(["hello", "hey"]);
+console.log(x)
