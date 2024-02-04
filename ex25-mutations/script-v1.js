031740b9883ab41f3ebce7b6ee54e16bf9c3904f
ameliapham <ptt.huong.8895@gmail.com>
2024-02-04 22:04:40 +0100

    Ex25 Mutations (method 1)

---
+++ ex25-mutations/script-v1.js
@@ -0,0 +1,26 @@
+// Return true if the string in the first element of the array 
+// contains all of the letters of the string in the second element of the array.
+
+function mutation(arr) {
+
+    const arr1 = arr[0].split("").map(char => char.toLowerCase())
+  
+    const arr2 = arr[1].split("").map(char => char.toLowerCase())
+  
+    let newArr2 = []
+  
+    for (let i = 0; i < arr2.length; i++) {
+      if (arr1.every(char => arr2[i] !== char)) {
+        newArr2.push(arr2[i]);
+      }
+    }
+    
+    if (newArr2.length === 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+  
+  const x = mutation(["hello", "hey"]);
+  console.log(x)
