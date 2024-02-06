0d26db03436b2b5161c740e93afcd3926607445b
ameliapham <ptt.huong.8895@gmail.com>
2024-02-06 22:02:27 +0100

    Ex27 Adding prototype properties to function "Cat"

--- Ex27-constructor-function/script.js
+++ Ex27-constructor-function/script.js
@@ -2,6 +2,16 @@ function Cat(name){
     this.name = name
 }
 
+Cat.prototype = {
+    numLegs: 4,
+    eat: function(){
+        console.log("miam miam miam")
+    },
+    describe: function(){
+        console.log(`My name is ${this.name}`)
+    }
+}
+
 const birman = new Cat("Shiny")
 
-console.log(birman.name)
+console.log(birman.describe())
