fc3fafe4a3f6a6b495c892af0ea3de87e51210dc
ameliapham <ptt.huong.8895@gmail.com>
2024-02-07 21:00:19 +0100

    Ex28 Update function by using indexOf

--- ex28-diff-two-array/script-v2.js
+++ ex28-diff-two-array/script-v2.js
@@ -1,7 +1,9 @@
 function diffArray(arr1, arr2) {
     let newArr = arr1.concat(arr2)
 
-    return newArr.filter(elem => !arr1.includes(elem) || !arr2.includes(elem))
+    // return newArr.filter(elem => !arr1.includes(elem) || !arr2.includes(elem))
+    return newArr.filter(elem => arr1.indexOf(elem) === -1 || arr2.indexOf(elem) === -1)
+
 }
 
 const x = diffArray([1, 2, 3, 5, 6, 7], [1, 2, 3, 4, 5]);
