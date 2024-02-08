08ca1560c9e7867c0e5f505c05423e02f0e3bb70
ameliapham <ptt.huong.8895@gmail.com>
2024-02-08 01:31:41 +0100

    Ex30 Pig Latin

---
+++ ex30-pig-latin/script.js
@@ -0,0 +1,11 @@
+function translatePigLatin(str) {
+
+    return str;
+}
+
+const x = translatePigLatin("schwartz") // -> artzschway
+console.log(x)
+translatePigLatin("rhythm"); // -> rhythm
+translatePigLatin("california") // -> aliforniacay
+translatePigLatin("glove") // -> oveglay
+translatePigLatin("algorithm") // -> algorithmway
