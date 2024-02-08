1384fc5efb01b433465a788994b1c4af51937659
ameliapham <ptt.huong.8895@gmail.com>
2024-02-08 02:27:01 +0100

    Ex31 Using method concat and filter

--- ex31-sorted-union/script.js
+++ ex31-sorted-union/script.js
@@ -1,11 +1,12 @@
-function uniteUnique(arr) {
+function uniteUnique(...arr) {
     
+    const newArr = [].concat(...arr)
 
-
-    return arr;
+    return  newArr.filter((elem, i, arr) => arr.indexOf(elem) === i)
 }
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]); // [1, 3, 2, 5, 4]
+const x = uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]); // [1, 3, 2, 5, 4]
+console.log(x)
 uniteUnique([1, 2, 3], [5, 2, 1]); // [1, 2, 3, 5]
 uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]); // [1, 2, 3, 5, 4, 6, 7, 8].
 uniteUnique([1, 3, 2], [5, 4], [5, 6]); // [1, 3, 2, 5, 4, 6].
